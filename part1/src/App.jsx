5615a3db1a5d252277ffe6bfe4f2e7423fbcf6af
Siraj <sirajseffarboujemaa@gmail.com>
2026-02-19 17:18:06 +0100

    finished exercise 1.7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,30 +4,52 @@ import { useState } from 'react'
 const Button = (props) => {
   return(
     <>
-      <button onClick={props.handleClick}>{props.text}</button>
+      <button onClick={props.handleClick }>{props.text}</button>
     </>
     
   )
 }
+
+
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [puntuation, setPuntuation] = useState(0)
+  const numberOfClicks = good + neutral + bad
 
+  const handleClickGood = () =>{
+    setGood(good+1)
+    setPuntuation(puntuation+1)
+  }
+
+  const handleClickNeutral = () =>{
+    setNeutral(neutral+1)
+    
+  }
+
+  const handleClickBad = () =>{
+    setBad(bad+1)
+    setPuntuation(puntuation-1)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick = {()=>{setGood(good+1)}} text="good"/>
-      <Button handleClick = {()=>{setNeutral(neutral+1)}} text="neutral" />
-      <Button handleClick = {()=>{setBad(bad+1)}} text="bad" />
+      <Button handleClick = {handleClickGood}  text="good" />
+      <Button handleClick = {handleClickNeutral}  text="neutral" />
+      <Button handleClick = {handleClickBad}  text="bad" />
+      
 
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good+neutral+bad}</p>
+      <p>average {puntuation/numberOfClicks}</p>
+      <p>positive {(good/numberOfClicks)*100}%</p>
     </div>
   )
 }
