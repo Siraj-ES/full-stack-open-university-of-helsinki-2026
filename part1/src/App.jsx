c80a0fbfa20850c2c0991b685c9048e8d02bbeb9
Siraj <sirajseffarboujemaa@gmail.com>
2026-02-27 11:03:52 +0100

    finished exercise 1.14

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -18,7 +18,7 @@ const History = (props) =>{
 const Button = (props) => {
   return(
     <>
-      <button onClick={props.handleClick }>{props.text}</button>
+      <button onClick={props.handleClick}>{props.text}</button>
     </>
     
   )
@@ -28,23 +28,47 @@ const Statistics = (props) =>{
   const average = props.all === 0 ? 0: props.puntuation/props.all
   const positive = props.all ===0 ? 0: (props.good / props.all)*100
   return(
-    <table>
-      <thead>
-        <tr>
-          <th>statistics</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr><StatisticLine value = {props.good} text ="good"/></tr>
-        <tr><StatisticLine value = {props.neutral} text ="neutral"/></tr>
-        <tr><StatisticLine value = {props.bad} text ="bad"/></tr>
-        <tr><StatisticLine value = {average} text ="average"/></tr>
-        <tr><StatisticLine value = {`${positive}%`} text ="positive"/></tr> 
-      </tbody>
-      
-    </table>
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <tr><StatisticLine value = {props.good} text ="good"/></tr>
+          <tr><StatisticLine value = {props.neutral} text ="neutral"/></tr>
+          <tr><StatisticLine value = {props.bad} text ="bad"/></tr>
+          <tr><StatisticLine value = {average} text ="average"/></tr>
+          <tr><StatisticLine value = {`${positive}%`} text ="positive"/></tr> 
+        </tbody>
+      </table>
+    </div>
+    
   )
 }
+
+
+const Anecdotes = (props) =>{
+  return(
+  <>
+    <h1>Anecdote of the day</h1>
+    <p>{props.anecdotes}</p>
+    
+  </>
+  )
+}
+
+
+const AnecdotesMax = (props) =>{
+  const {text, votes} = props.anecdoteMax
+
+  return(
+    <>
+      
+      <h1>Anecdote with most votes</h1>
+      <p>{text} has {votes} votes</p>
+    </>
+  )
+}
+
+
 
 const StatisticLine = (props) =>{
   return(
@@ -56,6 +80,14 @@ const StatisticLine = (props) =>{
   )
 }
 
+const Votes = (props) =>{
+  return(
+    <>
+      <p>has {props.votes} votes</p>
+    </>
+  )
+}
+
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
@@ -64,6 +96,20 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [puntuation, setPuntuation] = useState(0)
   const all = good + neutral + bad
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
   
   const handleClickGood = () =>{
     setGood(good+1)
@@ -79,7 +125,35 @@ const App = () => {
     setBad(bad+1)
     setPuntuation(puntuation-1)
   }
+  
+  const randomly = () =>{
+    let next = 0
+    next = Math.floor(Math.random()*anecdotes.length)%anecdotes.length
+    setSelected(next)
+  }
 
+  const handleVotes = () =>{
+    
+    let copy = [...votes]
+    copy[selected] +=1
+    setVotes(copy)
+
+  }
+
+  const handleMaxVote = ()=>{
+    let copy = [...votes]
+    let a = anecdotes[0]
+    let max = 0
+        copy.forEach((element, index) => {
+          
+          if(element>max){
+            max = element
+            a = anecdotes[index]
+          }
+        });
+    return {text: a, votes: max}
+  }
+  
   return (
     <div>
       <h1>give feedback</h1>
@@ -88,7 +162,17 @@ const App = () => {
       <Button handleClick = {handleClickBad}  text="bad" />
       <History good={good} neutral={neutral} bad={bad}
                   all = {all} puntuation={puntuation}/>
+
+      <Anecdotes anecdotes={anecdotes[selected]}/>
+      <Votes votes={votes[selected]}/>
+
+      <Button handleClick = {randomly} text="anecdotes"/>  
+      <Button handleClick = {handleVotes} text = "votes"/>   
+       
+      <AnecdotesMax anecdoteMax = {handleMaxVote()}/>
     </div>
+
+    
     
   )
 }
