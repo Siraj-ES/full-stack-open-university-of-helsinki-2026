8174c1a71f46c3b160a87257195886ad962ce281
Siraj <sirajseffarboujemaa@gmail.com>
2026-03-03 14:31:49 +0100

    finished exercise 2.4

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -23,20 +23,20 @@ const Header = (props) =>{
 
 const Content = (props) =>{
   
-  const total = props.parts.reduce((sum, part) => {
+   const total = props.parts.reduce((sum, part) => {
     return sum + part.exercises
-  }, 0)
+  }, 0) 
   
   return(
     <>
       <ul>
         {props.parts.map(element => {
-          
           return <Part name = {element.name} exercises = {element.exercises}
-                key = {element.id}/>
+                  key = {element.id}/>
           })
-          
         }
+      
+        
         <p>total of {total} exercises </p>
       </ul>
     </>
@@ -51,30 +51,57 @@ const Part = (props) =>{
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return( 
-    <Course course={course} />
+
+  return(
+    courses.map(element=>{
+      return <Course course={element} />
+    })
+    
   )
 }
 
