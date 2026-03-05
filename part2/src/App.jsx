ab5d484220e67b207a8eaf9e12042775561154cc
Siraj <sirajseffarboujemaa@gmail.com>
2026-03-05 18:28:18 +0100

    finished exercise 2.6

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,57 +1,49 @@
-import Course from './components/Course'
+import Note from './components/Note'
+import { useState } from 'react'
+
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
 
 
-  return(
+  const addName = (event) =>{
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    setPersons(persons.concat({name:newName}))
+    setNewName('')
+  }
+
+  const handleNewName = (event) => {
+    console.log(event.target)
+    setNewName(event.target.value)
+  }
+
+  
+
+  return (
     <div>
-      {courses.map(element=><Course course={element} key={element.id} />)}
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <>
+        {persons.map(element=>
+          <p key={element.name}>{element.name}</p>
+          
+        )}
+      </>
     </div>
   )
 }
+
 
 export default App
